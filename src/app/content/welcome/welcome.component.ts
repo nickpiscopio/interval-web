6e2ba4f89b64c4f45596b8fa9f3511919da1625e
Nick Piscopio <nick.piscopio@gmail.com>
2018-07-17 16:40:35 -0700

    Updated the animation on the welcome screen.

--- src/app/content/welcome/welcome.component.ts
+++ src/app/content/welcome/welcome.component.ts
@@ -2,10 +2,9 @@ import { Component, AfterContentInit } from '@angular/core';
 import { Route } from '../../constant/route.constant';
 import { trigger, style, transition, animate, state } from '@angular/animations';
 import { Router } from '../../../../node_modules/@angular/router';
+import { Class } from '../../constant/class.constant';
 
-const THRESHOLD_SHOW_BUTTON = 1000;
-const THRESHOLD_ANIMATION_CREATE_BUTTON = 800;
-const THRESHOLD_ANIMATION_TITLE = 300;
+const THRESHOLD_SHOW_BUTTON = 550;
 
 const STATE_IS_SHOWN = 'true';
 const STATE_ISNT_SHOWN = 'false';
@@ -15,26 +14,25 @@ const STATE_ISNT_SHOWN = 'false';
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.sass'],
   animations: [
-    trigger('buttonAnimation', [
-      transition(':enter', [style({ opacity: 0 }), animate(THRESHOLD_ANIMATION_CREATE_BUTTON, style({ opacity: 1 }))]),
-      transition(':leave', [style({ opacity: 1 }), animate(THRESHOLD_ANIMATION_CREATE_BUTTON, style({ opacity: 0 }))])
-    ]),
     trigger('titleAnimation', [
       state(STATE_IS_SHOWN, style({ transform: 'translateY(0)' })),
-      state(STATE_ISNT_SHOWN, style({ transform: 'translateY(22%)' })),
-      transition(STATE_IS_SHOWN + ' <=> ' + STATE_ISNT_SHOWN, animate(THRESHOLD_ANIMATION_TITLE))
+      state(STATE_ISNT_SHOWN, style({ transform: 'translateY(50%)' })),
+      transition(STATE_IS_SHOWN + ' <=> ' + STATE_ISNT_SHOWN, animate(THRESHOLD_SHOW_BUTTON / 2))
     ])
   ]
 })
 export class WelcomeComponent implements AfterContentInit {
-  showButton = false;
+  slideTitle = false;
+
+  buttonClass: string;
 
   constructor(private router: Router) {}
 
   ngAfterContentInit() {
     // We want to show the button after a certain threshold.
     setTimeout(() => {
-      this.showButton = true;
+      this.slideTitle = true;
+      this.buttonClass = Class.ACTIVE;
     }, THRESHOLD_SHOW_BUTTON);
   }
 
@@ -43,5 +41,12 @@ export class WelcomeComponent implements AfterContentInit {
    */
   navigateToCreateTimer() {
     this.router.navigate([Route.INTERNAL_ROUTE_TIMER_CREATE]);
+  }
+
+  /**
+   * Gets the button class to display the button with animations.
+   */
+  getButtonClass() {
+    return this.buttonClass;
   }
 }
