610dfbea1949232d3a25c81bc64fa37b2ba91349
Nick Piscopio <nick.piscopio@gmail.com>
2018-07-20 18:29:14 -0700

    Added the start to the duration.

--- src/app/content/timer/interval/interval.component.ts
+++ src/app/content/timer/interval/interval.component.ts
@@ -17,6 +17,12 @@ export class IntervalComponent {
   @Output() duplicateInterval = new EventEmitter<number>();
   @Output() removeInterval = new EventEmitter<number>();
 
+  private hours = '';
+  private minutes = '';
+  private seconds = '';
+
+  private time = ['0','0','0','0','0','0'];
+
   // @ViewChild('seconds') secondsInput: ElementRef;
 
   // private hours: number;
@@ -28,6 +34,57 @@ export class IntervalComponent {
    */
   onChange() {
     this.updateInterval.emit(this.index);
+  }
+
+  getValue(index: number) {
+    return this.time[index];
+  }
+
+  getModel(index: number) {
+
+  }
+
+  onChangeDuration(index: number) {
+    // This is the value at the index that was changed.
+    // We need to send this to the new index, and remove this to the index above.
+    let val0 = this.time[index][0];
+    let val1 = this.time[index][1];
+
+    if (val0 !== undefined && val1 !== undefined) {
+      this.time.splice(index+1, 0, val0)
+      this.time.splice(index+2, 0, val1)
+  
+      // Remove the first inex because we don't need it anymore.
+      this.time.shift();
+      this.time.shift();
+    }
+  }
+
+  onChangeSeconds() {
+    // this.hours = Math.floor(this.hours * 3600);
+    // this.minutes = Math.floor(this.minutes * 60);
+
+    // this.interval.duration -= this.seconds * 1000;
+    // let time = this.interval.duration / 1000;
+
+    // let hours = Math.floor(time / 3600);
+    // let minutes = Math.floor((time % 3600) / 60);
+
+    // this.parseTime(hours + minutes + this.seconds)
+
+    // if (this.seconds.length > 2) {
+    //   this.
+    // }
+  }
+
+  parseTime(time: number) {
+    // if (time > 0) {
+    //   this.hours = Math.floor(time / 3600);
+    //   this.minutes = Math.floor((time % 3600) / 60);
+    //   this.seconds = Math.floor((time % 3600) % 60);
+
+    //   this.interval.duration = time * 1000
+    // }   
   }
 
   /**
