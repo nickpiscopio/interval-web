a14b32447d6a1e513f519c3c110e40fb9179b18b
Nick Piscopio <nick.piscopio@gmail.com>
2018-07-23 12:44:11 -0700

    Added the empty menu.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // Router
@@ -29,19 +30,30 @@ import { IntervalComponent } from './content/timer/interval/interval.component';
 import { ClockDisplayComponent } from './fragments/clock-display/clock-display.component';
 
 @NgModule({
-  declarations: [AppComponent, WelcomeComponent, TimerComponent, TimerCreateComponent, FooterComponent, IntervalComponent, ClockDisplayComponent],
-  imports: [CommonModule,
-            BrowserModule,
-            FormsModule,
-            DragulaModule.forRoot(),
-            MatButtonModule,
-            MatCardModule,
-            MatFormFieldModule,
-            MatIconModule,
-            MatInputModule,
-            MatMenuModule,
-            BrowserAnimationsModule,
-            routes],
+  declarations: [
+    AppComponent,
+    WelcomeComponent,
+    TimerComponent,
+    TimerCreateComponent,
+    FooterComponent,
+    IntervalComponent,
+    ClockDisplayComponent
+  ],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    FormsModule,
+    DragulaModule.forRoot(),
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatMenuModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    routes
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
