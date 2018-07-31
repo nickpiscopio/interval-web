ee661fba34a210648a7e6410da607a0e45f124b9
Nick Piscopio <nick.piscopio@gmail.com>
2018-07-31 15:47:17 -0700

    Removed unneeded code.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,8 @@
 import { Component } from '@angular/core';
-import {NavigationEnd, Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  constructor(private router: Router) {
-    this.router.events.subscribe(event => {
-      if (event instanceof NavigationEnd) {
-        (<any>window).gtag('set', 'page', event.urlAfterRedirects);
-        (<any>window).gtag('send', 'pageview');
-      }
-    });
-  }
-}
+export class AppComponent {}
